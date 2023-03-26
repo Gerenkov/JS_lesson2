a4d852bd96993c38f38709dc7101d1d558b1c288
Gerenkov Vitalii <Gerenkov3830br@mail.ru>
2023-03-26 16:20:36 +0800

    save2

--- Фунции и объекты/js/script.js
+++ Фунции и объекты/js/script.js
@@ -25,7 +25,7 @@ function getArraysOddAndEvenCount() {
 getArraysOddAndEvenCount(); */
 
 // Задание 2
-function primeNumb(num) {
+/* function primeNumb(num) {
 	if (!Number.isInteger(num) || num > 1000) return "данные неверны";
 	if (num > 1) {
 		for (let i = 2; i < num; i++) {
@@ -39,4 +39,52 @@ function primeNumb(num) {
 	}
 }
 
-console.log(primeNumb(2000));
+console.log(primeNumb(5)); */
+
+// Задание 3
+/* function getSum(x) {
+	return function (y) {
+		return x + y;
+	};
+}
+
+let iternalFunction = getSum(2);
+console.log(iternalFunction(5)); */ //принимает число как аргумент и возвращает сумму этих двух чисел
+
+// Задание 4
+/* function numInterval(a, b) {
+	let i = setInterval(function () {
+		console.log(a++);
+		if (a > b) clearInterval(i);
+	}, 1000)
+}
+
+numInterval(5, 15); */
+
+// Задание 5
+// const pow = (x, n) => {
+// 	return x ** n;
+// }
+
+// console.log(pow(6, 3));
+
+// Задание 6
+/* const valueRandom = {
+	name: "Vitalii",
+	num: 20,
+	boolean: true,
+	func: function () {
+		console.log(2 + 2);
+	}
+};
+valueRandom.address = 'Bratsk';
+console.log(valueRandom);
+
+delete valueRandom.num;
+console.log(valueRandom);
+
+console.log('num' in valueRandom);
+
+for (let key in valueRandom) {
+	console.log(valueRandom[key]);
+} */
